472d6a02f7d4875313ac553e62ad8944f19765ef
Pingting Liu <liu.ping@northeastern.edu>
2023-11-16 15:48:34 -0800

    finished courses

--- app.js
+++ app.js
@@ -2,9 +2,19 @@
 import express from 'express';
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
 
 const app = express()
 app.use(express.json());
+app.use(cors());
+
+CourseRoutes(app);
+ModuleRoutes(app);
+
+
+
 Lab5(app);
 
 Hello(app)
