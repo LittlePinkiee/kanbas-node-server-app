0a9826c83101733c35ba5de576c89544a695f58c
Pingting Liu <liu.ping@northeastern.edu>
2023-12-01 17:31:02 -0800

    for deploy

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 // const express = require('express')
 import express from "express";
+import "dotenv/config";
 import session from "express-session";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-import UserRoutes from "./users/routes.js";
-
-import "dotenv/config";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   })
 );
 
@@ -27,6 +28,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 
 UserRoutes(app);
